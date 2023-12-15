50363940471ee941f425c185d189f13ec09b4b3c
DarshanaJK <128844702+DarshanaJK@users.noreply.github.com>
2023-12-15 12:58:22 +0530

    customer routes

--- app.js
+++ app.js
@@ -3,25 +3,14 @@ const app = express();
 const port = 3000;
 
 
+//import routes
+const customer = require('./routes/customer')
+
 //middleware
 app.use(express.json());
-app.get('/', (req,res) => {
-    res.send("<h1><center>Hello World</center></h1>")
-})
-
-app.get('/customers', (req,res) => {
-    res.send('Customers page');
-})
+app.use('/customers', customer);
 
 
-app.post('/', (req, res) => {
-    res.send('Got a POST request!');
-})
-
-app.post('/customers', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-})
 
 
 
