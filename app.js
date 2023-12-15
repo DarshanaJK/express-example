bc17987067d9bf73ac3f9f4ada27fb2a76741bcc
DarshanaJK <128844702+DarshanaJK@users.noreply.github.com>
2023-12-15 12:06:19 +0530

    use middleware

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+
+//middleware
+app.use(express.json());
 app.get('/', (req,res) => {
     res.send("<h1><center>Hello World</center></h1>")
+})
+
+app.get('/customers', (req,res) => {
+    res.send('Customers page');
 })
 
 
@@ -11,10 +18,11 @@ app.post('/', (req, res) => {
     res.send('Got a POST request!');
 })
 
-
-app.get('/customers', (req,res) => {
-    res.send('Customers page');
+app.post('/customers', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
 })
+
 
 
 app.listen(port, () => {
