5a40da0fbecf81126c379f175ce93903ceeb06bb
DarshanaJK <128844702+DarshanaJK@users.noreply.github.com>
2023-12-15 11:46:09 +0530

    Install nodemon and create post request

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req,res) => {
-    res.send('Hello World')
+    res.send("<h1><center>Hello World</center></h1>")
 })
+
+
+app.post('/', (req, res) => {
+    res.send('Got a POST request!');
+})
+
+
+app.get('/customers', (req,res) => {
+    res.send('Customers page');
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
